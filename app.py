dea653b69f26999eb827e5850625e3c16e0e07c4
Chetan3327 <chauhanchetan12789@gmail.com>
2023-12-15 23:48:51 +0530

    code completion chain

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from langcorn import create_service
+from fastapi.middleware.cors import CORSMiddleware
 
 app = create_service(
     'llm_chain:chain',
     'conversation_chain:conversation'
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
--- llm_chain.py
+++ llm_chain.py
@@ -9,12 +9,27 @@ load_dotenv()
 
 llm = GooglePalm(google_api_key=os.environ['GOOGLE_API_KEY'], temperature=0.2)
 
+
 prompt = PromptTemplate(
-    input_variables=['product'],
-    template='What is good name for a company name that makes {product} ?'
+    input_variables=['code'],
+    template="Don't write any markdown like ``` for code. Indent code with you are providing. Complete the following code:\n\n{code}"
 )
+
+# prompt = PromptTemplate(
+#     input_variables=['chapter_name', 'course_name', 'user_prompt'],
+#     template='Create a short compelling description for a chapter {chapter_name} of course named {course_name}.  {user_prompt}.'
+# )
 
 chain = LLMChain(llm=llm, prompt=prompt)
 
 if __name__ == '__main__':
-    print(chain.run('websites'))
+    input_data = {
+        "code": "const factorial = (n) => {    // inplement code for factorial of n }"
+    }
+
+    # input_data = {
+    #     "chapter_name": "sql for begginers",
+    #     "course_name": "sql",
+    #     "user_prompt": ""
+    # }
+    print(chain.run(**input_data))
