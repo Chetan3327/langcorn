72ce6c69b30d2928fd56dcc7aa314219c141f9a3
Chetan3327 <chauhanchetan12789@gmail.com>
2023-12-26 23:29:44 +0530

    conversation window of k=3

--- conversation_chain.py
+++ conversation_chain.py
@@ -1,6 +1,6 @@
 from langchain.chains import ConversationChain
 from langchain.chat_models import ChatGooglePalm
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder, SystemMessagePromptTemplate
 
 
@@ -11,14 +11,14 @@ load_dotenv()
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        SystemMessagePromptTemplate.from_template('The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.'),
+        SystemMessagePromptTemplate.from_template('The following is a friendly conversation between a human and an AI(name of AI is Luna). AI is good at Coding and provides good solutions to the questions.'),
         MessagesPlaceholder(variable_name='history'),
         HumanMessagePromptTemplate.from_template('{input}')
     ]
 )
 
 llm = ChatGooglePalm(google_api_key=os.environ['GOOGLE_API_KEY'], temperature=0.2)
-memory = ConversationBufferMemory(return_messages=True)
+memory = ConversationBufferWindowMemory(k=3, return_messages=True)
 conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm)
 
 if __name__ == '__main__':
